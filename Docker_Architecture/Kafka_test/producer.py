2a27e6312dafcef2b15edd398bac227d12eeadba
MiSalah <elbaidoury.salaheddine@gmail.com>
2022-01-08 14:14:39 +0000

    Fake data to Kafka topic for testing

---
+++ Docker_Architecture/Kafka_test/producer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaProducer
+import json
+import time as t
+from data import get_users
+
+def json_serialize(data):
+    return json.dumps(data)
+
+
+producer = KafkaProducer(
+    bootstrap_servers=["localhost:9092"],
+    value_serializer=json_serialize)
+
+if __name__ == '__main__':
+    while 1==1:
+        users = get_users()
+        print(users)
+        producer.send("users",users)
+        t.sleep(3)
