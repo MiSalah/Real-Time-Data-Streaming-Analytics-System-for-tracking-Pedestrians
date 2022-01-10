00c57a41ca5be83502fc4bb1792a47cc9c0f5b53
MiSalah <elbaidoury.salaheddine@gmail.com>
2022-01-10 21:39:49 +0000

    push it all

--- Docker_Architecture/Kafka_test/consumer.py
+++ Docker_Architecture/Kafka_test/consumer.py
@@ -1,5 +1,5 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('positions', bootstrap_servers=['localhost:9092'])
-
+print("hello")
 for message in consumer:
-    print (message)
+    print(message)
